caa2825427bca3a76d20173fbc26196c1273baa2
David Aurelio <david@uxebu.com>
2011-11-23 14:31:47 +0100

    Initial Commit

---
+++ cameradump/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cameradump
+//
+//  Created by David Aurelio on 23.11.11.
+//  Copyright (c) 2011 uxebu Consulting Ltd. & Co. KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
